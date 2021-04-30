2a67db7d049c26af3500403ad74c9889094ae2ec
Touis Abdou <touisabdelbasset@gmail.com>
2021-04-30 23:32:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Photo from './Component/Profile/ProfilPhoto';
+import Name from './Component/Profile/FullName';
+import Address from './Component/Profile/Address';
+
+function App() {
+  return (
+    <div id="root" className="App">
+      <Photo></Photo>
+     
+      <Name></Name>
+      <Address></Address>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function Name(){
+    return <p >Touis Abdelbasset</p>
+}
+export default Name
